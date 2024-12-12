94b85eef48cba6b8b3955734202053994674bea7
palascinochristian <palascinochristian@gmail.com>
2024-12-12 20:08:33 +0100

    feat: add randomNumGen function in Snack2

--- Snack 2/script.js
+++ Snack 2/script.js
@@ -21,3 +21,14 @@ const soccerSquads  = [
 
     }
 ];
+
+//Funzione per numeri randomici dati due numeri in intervallo
+const randomNumGen = (min, max) => {
+    const Num = Math.floor(Math.random() * (max - min + 1)) + min;
+    return Num;
+}
+
+//Salvo i valori casuali a delle variabili riutilizzabili richiamando la funzione
+const points = randomNumGen(1, 60);
+const fails = randomNumGen (1, 10);
+
