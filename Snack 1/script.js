fe058eb10505ed480af281d7f69fbe64492580ff
palascinochristian <palascinochristian@gmail.com>
2024-12-12 19:57:38 +0100

    added for cycle + first exercise end

--- Snack 1/script.js
+++ Snack 1/script.js
@@ -1,4 +1,5 @@
 
+
 // Dichiaro dentro la variabile bikes , un array di 3 oggetti
 const bikes = [
     {
@@ -14,3 +15,15 @@ const bikes = [
         weight: 8.3
     }
 ]
+// Creo la variabile bici più leggera
+let lightestBike = bikes[0];
+
+//Attraverso l'array con un ciclo for che itera per la sua lunghezza, confrontando l'etichetta weight
+for (let i = 1; i < bikes.length; i++){
+    if(bikes[i].weight < lightestBike.weight){
+        lightestBike = bikes[i]; //Aggiorno di volta in volta il valore di lightesBike con l'indice dell'array 
+
+    }
+}
+// Stampo in console il nome della bici col peso minore
+console.log(`La bici più leggera è ${lightestBike.brand}`)
