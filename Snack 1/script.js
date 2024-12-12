e62eb011fd129af001720cee5d668de8c2c26b1f
palascinochristian <palascinochristian@gmail.com>
2024-12-12 19:28:19 +0100

    feat: added bikes objects array

---
+++ Snack 1/script.js
@@ -0,0 +1,16 @@
+
+// Dichiaro dentro la variabile bikes , un array di 3 oggetti
+const bikes = [
+    {
+        brand: "Specialized Tarmac SL7",
+        weight: 6.8
+    },
+    {
+        brand: "Canyon",
+        weight: 6.2
+    },
+    {
+        brand: "Cinelli",
+        weight: 8.3
+    }
+]
