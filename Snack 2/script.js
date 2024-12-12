eb327c1edfb223476670ad5173eeb20cedeb49f2
palascinochristian <palascinochristian@gmail.com>
2024-12-12 20:02:28 +0100

    init snack 2 + added objects array

---
+++ Snack 2/script.js
@@ -0,0 +1,23 @@
+
+
+// Creo un array che contenga come oggetti delle squadre di calcio
+
+const soccerSquads  = [
+    {
+        squadName: "Juventus",
+        points: 0,
+        fails: 0
+    },
+    {
+        squadName: "Milan",
+        points: 0,
+        fails: 0
+
+    },
+    {
+        squadName: "Inter",
+        points: 0,
+        fails: 0
+
+    }
+];
